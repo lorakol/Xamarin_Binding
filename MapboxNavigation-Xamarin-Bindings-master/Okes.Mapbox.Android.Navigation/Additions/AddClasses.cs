ff9178afd9f22f32b25129d1cf7f685b1ce73112
oldmansjj <oldmanfirst@outlook.com>
2020-06-08 17:07:22 +0300

    first commit

---
+++ MapboxNavigation-Xamarin-Bindings-master/Okes.Mapbox.Android.Navigation/Additions/AddClasses.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Mapbox.Services.Android.Navigation.V5.Milestone
+{
+    partial class RouteMilestone
+    {
+        partial class Builder
+        {
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone Build()
+            {
+                return Build1();
+            }
+
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone.Builder SetTrigger(global::Com.Mapbox.Services.Android.Navigation.V5.Milestone.Trigger.Statement trigger)
+            {
+                return SetTrigger1(trigger);
+            }
+        }
+    }
+    partial class StepMilestone
+    {
+        partial class Builder
+        {
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone Build()
+            {
+                return Build1();
+            }
+
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone.Builder SetTrigger(global::Com.Mapbox.Services.Android.Navigation.V5.Milestone.Trigger.Statement trigger)
+            {
+                return SetTrigger1(trigger);
+            }
+        }
+    }
+    partial class VoiceInstructionMilestone
+    {
+        partial class Builder
+        {
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone Build()
+            {
+                return Build1();
+            }
+
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone.Builder SetTrigger(global::Com.Mapbox.Services.Android.Navigation.V5.Milestone.Trigger.Statement trigger)
+            {
+                return SetTrigger1(trigger);
+            }
+        }
+    }
+}
+
+namespace Com.Mapbox.Services.Android.Navigation.V5.Milestone
+{
+    partial class BannerInstructionMilestone
+    {
+        partial class Builder
+        {
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone Build()
+            {
+                return Build1();
+            }
+
+            public override Com.Mapbox.Services.Android.Navigation.V5.Milestone.Milestone.Builder SetTrigger(global::Com.Mapbox.Services.Android.Navigation.V5.Milestone.Trigger.Statement trigger)
+            {
+                return SetTrigger1(trigger);
+            }
+        }
+    }
+}
